76030e3f1ed4883298f2f3e2215e92f3919e7529
sage90180 <sage90180@gmail.com>
2020-11-12 09:17:01 +0800

    pretty

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { ResetStyle, GlobalStyle } from './globalStyle';
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import { ResetStyle, GlobalStyle } from "./globalStyle";
+import App from "./App";
 ReactDOM.render(
   <>
     <ResetStyle />
     <GlobalStyle />
     <App />
-  </>
-  ,
-  
-  document.getElementById('root')
+  </>,
+  document.getElementById("root")
 );
